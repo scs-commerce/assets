a86a86da04d911723efe98b6599884818c575535
Falk Hoppe <falkhoppe81@gmail.com>
2017-02-13 08:37:55 +0100

    serve assets

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(express.static('node_modules'))
+app.use(express.static('dist'))
+
+app.use((req, res, next) =>
+  res.status(404).render('404'))
+
+app.use((error, req, res, next) => {
+  console.log(error)
+  res.status(500).render('500', { error })
+})
+
+console.log(`start listening on port ${process.env.PORT}`)
+app.listen(process.env.PORT)
